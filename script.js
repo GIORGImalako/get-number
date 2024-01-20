e405f77721bd34a2b8cb8b3d7770b158963e9aa6
Giorgi Malakmadze <malakmadzegio5@gmail.com>
2024-01-20 19:44:41 +0400

    get the number

---
+++ script.js
@@ -0,0 +1,13 @@
+//for(initialization, condition, iteration)
+for (let i = 0; i <= 11; i++) {
+    document.write("<h2> ეს კოდი განმეორდა  ${i}-ჯერ</h2>")
+}
+
+for(var x = 0; x < 10; x++) {
+    console.log(' this is loop ${x}')
+}
+    while (condition)   
+
+    do{
+        
+    }
